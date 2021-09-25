20e12ca124c2bbbd9d05c9cc9382bd9c02020a45
methe ezzitouni <methe@walo.app>
2021-09-25 17:16:15 +0100

    added files

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+document.addEventListener('click', () =>{
+    document.querySelector('#button').setAttribute('disabled', true)
+    alert('🤣, you would never be able to click it again, bad joke ...')  
+})
